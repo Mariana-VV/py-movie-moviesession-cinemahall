cef8ead8a9c5ebd6c2fff78d140152ff12be61d2
Mariana Trinko <marjanavv@gmail.com>
2026-02-26 16:49:23 +0100

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -48,7 +48,7 @@ def update_movie_session(session_id: int,
     if movie_id:
         session.movie_id = movie_id
     if cinema_hall_id:
-        session.cinema_hall = cinema_hall_id
+        session.cinema_hall_id = cinema_hall_id
 
     session.save()
 
