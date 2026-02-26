433adfaf2849defffca05a30b3f55baf6aea004b
Mariana Trinko <marjanavv@gmail.com>
2026-02-26 16:14:42 +0100

    Solution

--- services/movie_session.py
+++ services/movie_session.py
@@ -9,8 +9,8 @@ from db.models import MovieSession, CinemaHall, Movie
 def create_movie_session(movie_show_time: datetime,
                          movie_id: int,
                          cinema_hall_id: int) -> MovieSession:
-    cinema_hall=CinemaHall.objects.get(id=cinema_hall_id)
-    movie=Movie.objects.get(id=movie_id)
+    cinema_hall = CinemaHall.objects.get(id=cinema_hall_id)
+    movie = Movie.objects.get(id=movie_id)
     new_movie_session = MovieSession.objects.create(
         show_time=movie_show_time,
         cinema_hall=cinema_hall,
